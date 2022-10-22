572eaf5ee57d52541cba1fef275a7c7058779207
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-10-22 16:33:00 -0230

    calendar grid setup

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,15 +1,52 @@
 import React from 'react'
+import "tailwindcss/tailwind.css";
 
 export default function Calendar() {
   
   return (
-    <div className="container">
-      <div className="item">1</div>test
-      <div className="item">2</div>test2
-      <div className="item">3</div>test3
-      <div className="item">4</div>
-      <div className="item">5</div>
-      <div className="item">6</div>
+    <div className='grid grid-cols-7'>
+      <div class="cell col-span-7">November</div>
+      <div class="cell text-sky-400"></div>
+      <div class="cell"></div>
+      <div class="cell">1</div>
+      <div class="cell">2</div>
+      <div class="cell">3</div>
+      <div class="cell">4</div>
+      <div class="cell">5</div>
+      <div class="cell">6</div>
+      <div class="cell">7</div>
+      <div class="cell">8</div>
+      <div class="cell">9</div>
+      <div class="cell">10</div>
+      <div class="cell">11</div>
+      <div class="cell">12</div>
+      <div class="cell">13</div>
+      <div class="cell">14</div>
+      <div class="cell">15</div>
+      <div class="cell">17</div>
+      <div class="cell">18</div>
+      <div class="cell">19</div>
+      <div class="cell">20</div>
+      <div class="cell">21</div>
+      <div class="cell">22</div>
+      <div class="cell">23</div>
+      <div class="cell">24</div>
+      <div class="cell">25</div>
+      <div class="cell">26</div>
+      <div class="cell">27</div>
+      <div class="cell">28</div>
+      <div class="cell">29</div>
+      <div class="cell">30</div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
     </div>
   )
 }
