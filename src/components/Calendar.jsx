ef61ea7299fa5fbb959afd9cd5360407505b05dd
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-10-23 14:04:18 -0230

    Update Calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -5,48 +5,48 @@ export default function Calendar() {
   
   return (
     <div className='grid grid-cols-7'>
-      <div class="cell col-span-7">November</div>
-      <div class="cell text-sky-400"></div>
-      <div class="cell"></div>
-      <div class="cell">1</div>
-      <div class="cell">2</div>
-      <div class="cell">3</div>
-      <div class="cell">4</div>
-      <div class="cell">5</div>
-      <div class="cell">6</div>
-      <div class="cell">7</div>
-      <div class="cell">8</div>
-      <div class="cell">9</div>
-      <div class="cell">10</div>
-      <div class="cell">11</div>
-      <div class="cell">12</div>
-      <div class="cell">13</div>
-      <div class="cell">14</div>
-      <div class="cell">15</div>
-      <div class="cell">17</div>
-      <div class="cell">18</div>
-      <div class="cell">19</div>
-      <div class="cell">20</div>
-      <div class="cell">21</div>
-      <div class="cell">22</div>
-      <div class="cell">23</div>
-      <div class="cell">24</div>
-      <div class="cell">25</div>
-      <div class="cell">26</div>
-      <div class="cell">27</div>
-      <div class="cell">28</div>
-      <div class="cell">29</div>
-      <div class="cell">30</div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
+      <div class="cell col-span-7" id='month'>November</div>
+      <div class="cell" id='cell-2'></div>
+      <div class="cell" id='cell-3'></div>
+      <div class="cell" id='cell-4'>1</div>
+      <div class="cell" id='cell-5'>2</div>
+      <div class="cell" id='cell-6'>3</div>
+      <div class="cell" id='cell-7'>4</div>
+      <div class="cell" id='cell-8'>5</div>
+      <div class="cell" id='cell-9'>6</div>
+      <div class="cell" id='cell-10'>7</div>
+      <div class="cell" id='cell-11'>8</div>
+      <div class="cell" id='cell-12'>9</div>
+      <div class="cell" id='cell-13'>10</div>
+      <div class="cell" id='cell-14'>11</div>
+      <div class="cell" id='cell-15'>12</div>
+      <div class="cell" id='cell-16'>13</div>
+      <div class="cell" id='cell-17'>14</div>
+      <div class="cell" id='cell-18'>15</div>
+      <div class="cell" id='cell-19'>17</div>
+      <div class="cell" id='cell-20'>18</div>
+      <div class="cell" id='cell-21'>19</div>
+      <div class="cell" id='cell-22'>20</div>
+      <div class="cell" id='cell-23'>21</div>
+      <div class="cell" id='cell-24'>22</div>
+      <div class="cell" id='cell-25'>23</div>
+      <div class="cell" id='cell-26'>24</div>
+      <div class="cell" id='cell-27'>25</div>
+      <div class="cell" id='cell-28'>26</div>
+      <div class="cell" id='cell-29'>27</div>
+      <div class="cell" id='cell-30'>28</div>
+      <div class="cell" id='cell-31'>29</div>
+      <div class="cell" id='cell-32'>30</div>
+      <div class="cell" id='cell-33'></div>
+      <div class="cell" id='cell-34'></div>
+      <div class="cell" id='cell-35'></div>
+      <div class="cell" id='cell-36'></div>
+      <div class="cell" id='cell-37'></div>
+      <div class="cell" id='cell-38'></div>
+      <div class="cell" id='cell-39'></div>
+      <div class="cell" id='cell-40'></div>
+      <div class="cell" id='cell-41'></div>
+      <div class="cell" id='cell-42'></div>
     </div>
   )
 }
