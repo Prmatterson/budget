1b62353c22b93266696b28e175af21adaf4c33ab
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-10-25 21:11:37 -0230

    Update Calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,52 +1,23 @@
 import React from 'react'
-import "tailwindcss/tailwind.css";
+import "tailwindcss/tailwind.css"
 
 export default function Calendar() {
-  
+
   return (
-    <div className='grid grid-cols-7'>
-      <div class="cell col-span-7" id='month'>November</div>
-      <div class="cell" id='cell-2'></div>
-      <div class="cell" id='cell-3'></div>
-      <div class="cell" id='cell-4'>1</div>
-      <div class="cell" id='cell-5'>2</div>
-      <div class="cell" id='cell-6'>3</div>
-      <div class="cell" id='cell-7'>4</div>
-      <div class="cell" id='cell-8'>5</div>
-      <div class="cell" id='cell-9'>6</div>
-      <div class="cell" id='cell-10'>7</div>
-      <div class="cell" id='cell-11'>8</div>
-      <div class="cell" id='cell-12'>9</div>
-      <div class="cell" id='cell-13'>10</div>
-      <div class="cell" id='cell-14'>11</div>
-      <div class="cell" id='cell-15'>12</div>
-      <div class="cell" id='cell-16'>13</div>
-      <div class="cell" id='cell-17'>14</div>
-      <div class="cell" id='cell-18'>15</div>
-      <div class="cell" id='cell-19'>17</div>
-      <div class="cell" id='cell-20'>18</div>
-      <div class="cell" id='cell-21'>19</div>
-      <div class="cell" id='cell-22'>20</div>
-      <div class="cell" id='cell-23'>21</div>
-      <div class="cell" id='cell-24'>22</div>
-      <div class="cell" id='cell-25'>23</div>
-      <div class="cell" id='cell-26'>24</div>
-      <div class="cell" id='cell-27'>25</div>
-      <div class="cell" id='cell-28'>26</div>
-      <div class="cell" id='cell-29'>27</div>
-      <div class="cell" id='cell-30'>28</div>
-      <div class="cell" id='cell-31'>29</div>
-      <div class="cell" id='cell-32'>30</div>
-      <div class="cell" id='cell-33'></div>
-      <div class="cell" id='cell-34'></div>
-      <div class="cell" id='cell-35'></div>
-      <div class="cell" id='cell-36'></div>
-      <div class="cell" id='cell-37'></div>
-      <div class="cell" id='cell-38'></div>
-      <div class="cell" id='cell-39'></div>
-      <div class="cell" id='cell-40'></div>
-      <div class="cell" id='cell-41'></div>
-      <div class="cell" id='cell-42'></div>
+    <div>
+
+
+      <div className='grid grid-cols-7'>
+        <div class="cell col-span-7" id='month'>November</div>
+        <div id='date-header-sunday'>Sun</div>
+        <div id='date-header-monday'>Mon</div>
+        <div id='date-header-tuesday'>Tue</div>
+        <div id='date-header-wednesday'>Wed</div>
+        <div id='date-header-thursday'>Thu</div>
+        <div id='date-header-friday'>Fri</div>
+        <div id='date-header-saturday'>Sat</div>
+      </div>
+      
     </div>
   )
 }
