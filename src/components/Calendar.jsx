14a7f0237f97e503c4d1c6d445709e1aca0cfd36
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-10-25 21:14:02 -0230

    Update Calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,9 +3,13 @@ import "tailwindcss/tailwind.css"
 
 export default function Calendar() {
 
+  const day = [];
+  for (let x = 1; x < 31; x++) {
+    day.push(x);
+  };
+
   return (
     <div>
-
 
       <div className='grid grid-cols-7'>
         <div class="cell col-span-7" id='month'>November</div>
@@ -16,8 +20,14 @@ export default function Calendar() {
         <div id='date-header-thursday'>Thu</div>
         <div id='date-header-friday'>Fri</div>
         <div id='date-header-saturday'>Sat</div>
+        {day.map((number) => {
+          if (number === 1) { // Make this the first day of whatever month this is
+            return <div className="cell col-start-2">{number}</div>;
+          }
+          return <div className="cell">{number}</div>;
+        })}
       </div>
-      
     </div>
+
   )
 }
