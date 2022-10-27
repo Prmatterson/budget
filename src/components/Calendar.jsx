1df7b902dfec96d01d8c082c79ffd1a1ae71e100
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-10-26 21:30:02 -0230

    Update Calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -5,7 +5,10 @@ import "tailwindcss/tailwind.css"
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const todaysDate = new Date();
 const thisYear = todaysDate.getFullYear();
-const thisMonth = months[todaysDate.getMonth()];
+const thisMonthName = months[todaysDate.getMonth()]
+console.log(thisMonthName)
+const thisMonthIndex = months.findIndex({thisMonthName}) // Figure this out!!!!
+console.log(thisMonthIndex)
 
 // Preparation to assign which column the first day goes into (following two lines select the 1st day of the relevant month (e.g. "6" = Sat))
 const selectFirstDay = todaysDate.setDate(1);
@@ -15,12 +18,15 @@ const firstDay = todaysDate.getDay(); // the first column will be this index num
 function daysInMonth(year, month) {
   return new Date(year, month, 0).getDate()
 }
+console.log(daysInMonth(2022, 10))
 
 // Creating array of days in the month
 const day = [];
-for (let x = 1; x < daysInMonth(thisYear, thisMonth); x++) {
+for (let x = 1; x < daysInMonth(thisYear, thisMonthIndex); x++) {
   day.push(x);
 }
+console.log(day) // This is currently empty
+
 
 // Main calendar component
 export default function Calendar() {
@@ -30,7 +36,7 @@ export default function Calendar() {
 
       <div className='grid grid-cols-7'>
         <div class='cell col-span-7' id='year'> {thisYear} </div>
-        <div class="cell col-span-7" id='month'> {thisMonth} </div>
+        <div class="cell col-span-7" id='month'> {thisMonthName} </div>
         <div id='date-header-sunday'>Sun</div>
         <div id='date-header-monday'>Mon</div>
         <div id='date-header-tuesday'>Tue</div>
@@ -38,8 +44,14 @@ export default function Calendar() {
         <div id='date-header-thursday'>Thu</div>
         <div id='date-header-friday'>Fri</div>
         <div id='date-header-saturday'>Sat</div>
-        {/* {day.map(firstDay)} */}
+        {day.map((number) => {
+          if (number === 1) {
+            return <div className="cell col-start-2">{number}</div>;
+          } else {
+          return <div className="cell">{number}</div>;
+        }})}
       </div>
     </div>
-  )
+  ) 
 }
+
