7ab69268cc4c4c7d5adb2147f7a088453640a7bb
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-10-30 10:10:26 -0230

    first day offset

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,28 +4,31 @@ import "tailwindcss/tailwind.css"
 // Creating references to the year and month
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const todaysDate = new Date();
+console.log(todaysDate)
 const thisYear = todaysDate.getFullYear();
+console.log(thisYear)
+const thisMonthNumber = todaysDate.getMonth() + 1;
+console.log(thisMonthNumber)
 const thisMonthName = months[todaysDate.getMonth()]
 console.log(thisMonthName)
-const thisMonthIndex = months.findIndex({thisMonthName}) // Figure this out!!!!
-console.log(thisMonthIndex)
 
 // Preparation to assign which column the first day goes into (following two lines select the 1st day of the relevant month (e.g. "6" = Sat))
 const selectFirstDay = todaysDate.setDate(1);
 const firstDay = todaysDate.getDay(); // the first column will be this index number (e.g. Saturday (or "6") will be mapped to [6] (the 7th column))
+console.log(firstDay);
 
 // Function to automatically determine number of days in current month
 function daysInMonth(year, month) {
   return new Date(year, month, 0).getDate()
 }
-console.log(daysInMonth(2022, 10))
+console.log(daysInMonth(thisYear, thisMonthNumber))
 
 // Creating array of days in the month
 const day = [];
-for (let x = 1; x < daysInMonth(thisYear, thisMonthIndex); x++) {
+for (let x = 1; x < daysInMonth(thisYear, thisMonthNumber) + 1; x++) {
   day.push(x);
 }
-console.log(day) // This is currently empty
+console.log(day)
 
 
 // Main calendar component
@@ -46,12 +49,26 @@ export default function Calendar() {
         <div id='date-header-saturday'>Sat</div>
         {day.map((number) => {
           if (number === 1) {
-            return <div className="cell col-start-2">{number}</div>;
-          } else {
+            if (firstDay === 0) {
+              return <div className="cell">{number}</div>;
+            } else if (firstDay === 1) {
+              return <div className="cell col-start-2">{number}</div>
+            } else if (firstDay === 2) {
+              return <div className="cell col-start-3">{number}</div>
+            } else if (firstDay === 3) {
+              return <div className="cell col-start-4">{number}</div>
+            } else if (firstDay === 4) {
+              return <div className="cell col-start-5">{number}</div>
+            } else if (firstDay === 5) {
+              return <div className="cell col-start-6">{number}</div>
+            } else {
+              return <div className="cell col-start-7">{number}</div>
+            }
+          }
           return <div className="cell">{number}</div>;
-        }})}
+        })}
       </div>
     </div>
-  ) 
+  )
 }
 
