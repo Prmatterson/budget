0dc0d1ec880a48e2834c4b4ad42a3e7d3e81d87e
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-11-03 21:11:15 -0230

    form dropdowns added using useStatae

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,33 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Form() {
 
-function setTwoNumberDecimal(event) {
-    this.value = parseFloat(this.value).toFixed(2);
-}
+  // Array of objects containing data for form fields
+  let accounts = [
+    { label: "BMO", value: "Bank of Montreal" },
+    { label: "SB", value: "Scotiabank" },
+  ]
 
+  let frequencies = [
+    { label: "Once", value: "Once" },
+    { label: "Monthly", value: "Monthly" },
+    { label: "Biweekly", value: "Biweekly" },
+    { label: "Weekly", value: "Weekly" },
+    { label: "Yearly", value: "Yearly" },
+    { label: "Quarterly", value: "Quarterly" },
+    { label: "Biannually", value: "Biannually" },
+    { label: "Custom", value: "Custom" },
+  ]
+
+  // Using state to keep track of what the selected account/frequency is
+  let [account, setAccount] = useState("Select Account")
+  let [frequency, setFrequency] = useState("Select Frequency")
+
+  // Using this function to update the state of account/frequency
+  // whenever a new option is selected from the dropdown
+  let handleAccountChange = (e) => {
+    setAccount(e.target.value)
+  }
+  let handleFrequencyChange = (e) => {
+    setFrequency(e.target.value)
+  }
+
+  // Main Form Component
   return (
     <div id="form" className="mx-10">
       <form>
         <label>
           <label className="block text-sm font-medium text-slate-700">Amount</label>
-          <input type="number" name="amount-field" className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-xl text-sm shadow-xl"/>
-          <br/>
+
+          <br />
           <label className="block text-sm font-medium text-slate-700">Account</label>
-          <input type="text" name="amount-field" className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-xl text-sm shadow-xl"/>
-          <br/>
-          <label className="w-1/2 text-sm font-medium text-slate-700">Frequency</label>
-          <select type="text" name="amount-field" className="mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-xl text-sm shadow-xl">
-            <option id="option=once">Once</option>
-            <option id="option=monthly">Monthly</option>
-            <option id="option=biweekly">Biweekly</option>
-            <option id="option-weekly">Weekly</option>
-            <option id="option=yearly">Yearly</option>
-            <option id="option=quarterly">Quarterly</option>
-            <option id="option=biannually">Biannually</option>
-            <option id="option=custom">Custom</option>
+          <select className="mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-xl text-sm shadow-xl" onChange={handleAccountChange}>
+            <option value="Select Account"></option>
+            {accounts.map((account) => <option value={account.value}>{account.label}</option>)}
           </select>
-          <br/>
+          <br />
+          <label className="w-1/2 text-sm font-medium text-slate-700">Frequency</label>
+          <select className="mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-xl text-sm shadow-xl" onChange={handleFrequencyChange}>
+            <option value="Select Account"></option>
+            {frequencies.map((frequency) => <option value={frequency.value}>{frequency.label}</option>)}
+          </select>
+          <br />
         </label>
       </form>
 
