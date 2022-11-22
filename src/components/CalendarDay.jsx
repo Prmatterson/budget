32a09d1324a371db76657e1c538436e017f8cb94
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-11-21 21:21:56 -0330

    Update CalendarDay.jsx

--- src/components/CalendarDay.jsx
+++ src/components/CalendarDay.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export default function CalendarData({ day, calendarData }) {
+export default function CalendarDay({ day, calendarData }) {
     const events = [];
     // Use switch statement to go between frequencies and do the math
     // calendarData is an array, so need to use for loop to get at data within it
-    if (day.getDay() === 1) {
-        events.push(<div key={day.getDay()}>Test</div>)
+    switch (calendarData) {
+        case once:
+        if (day.getDay() === startDate) {
+            events.push(<div key={day.getDay()}>Test</div>)
+        }
     }
-    return events
 }
