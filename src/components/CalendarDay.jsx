af77fab5ea8084904b8b845c1f3f78051bc56c85
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-11-21 21:25:34 -0330

    Update CalendarDay.jsx

--- src/components/CalendarDay.jsx
+++ src/components/CalendarDay.jsx
@@ -5,7 +5,7 @@ export default function CalendarDay({ day, calendarData }) {
     // Use switch statement to go between frequencies and do the math
     // calendarData is an array, so need to use for loop to get at data within it
     switch (calendarData) {
-        case once:
+        case 'Once':
         if (day.getDay() === startDate) {
             events.push(<div key={day.getDay()}>Test</div>)
         }
