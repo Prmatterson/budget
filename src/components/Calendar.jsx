ed749c4c6d35de6cb5a94be2f6866d3035760470
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-12-06 21:04:06 -0330

    minor adj

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CalendarDay from './CalendarDay'
+import PrevMonth from './PrevMonth'
 
 // Creating references to the year and month
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
--- src/components/PrevMonth.jsx
+++ src/components/PrevMonth.jsx
@@ -3,11 +3,14 @@ import React from "react";
 export default function PrevMonth({}) {
 
   return (
+      
     <button onClick={() => {
         const currentDate = new Date();
         currentDate.setMonth(currentDate.getMonth()-1);
         const previousMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1)
-       console.log(previousMonth.getMonth())
+        thisMonthNumber = previousMonth
+        console.log(previousMonth.getMonth())
+       // Alternative, find function to change thisMonthNumber to "thisMonthNumber - 1"
       }}
     >
       Previous
