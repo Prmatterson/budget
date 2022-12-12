e6fa10c097f16076b89c3deade0c1a6f9eae4b22
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-12-12 20:26:39 -0330

    Update CashFlow.jsx

--- src/components/CashFlow.jsx
+++ src/components/CashFlow.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
+
+const fakeData = [
+    { accountName: "BMO", costs: [50, 100, 200] },
+    { accountName: "Scotia", costs: [50, 100, 200] },
+  ];
 
 export default function CashFlow() {
     const { register, handleSubmit } = useForm();
@@ -7,15 +12,17 @@ export default function CashFlow() {
       console.log(data);
       setCashFlowData((prev) => [...prev, data]);
     };
+    
+    const { cashFlowData, setCashFlowData} = useState()
 
 return (
     <div className="grid grid-cols-3 grid-rows-10" id='container'>
         <div className="col-span-4 align-center" id="cashflow-title">Cash Flow</div>
         <form className="col-span-4" id='from-to' onSubmit={handleSubmit(onSubmit)}>
             <label className="col-span- text-sm font-medium text-slate-700">Start Date</label>
-            <input type="date" className="col-span-1 mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-xl text-sm shadow-xl text-center" {...register("startDate")} />
+            <input type="date" className="col-span-1 mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-xl text-sm shadow-xl text-center" {...register("startDateCF")} />
             <label className="col-span-1 text-sm font-medium text-slate-700">End Date</label>
-            <input type="date" className="col-span-1 mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-xl text-sm shadow-xl text-center" {...register("endDate")} />
+            <input type="date" className="col-span-1 mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-xl text-sm shadow-xl text-center" {...register("endDateCF")} />
             <button type="submit" className="col-span 1mt-1 w-1/4 px-3 py-2 bg-white border border-4 border-slate-300 rounded-xl text-sm font-bold shadow-xl text-center hover:bg-slate-300">Submit</button>
         </form>
         <div className="col-span-1" id="cashflow-heading">Date</div>
