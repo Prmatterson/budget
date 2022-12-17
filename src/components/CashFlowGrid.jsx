52c005a56053fa52b83870409c1c429d291a27c7
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-12-17 13:29:00 -0330

    got cashFlow grid to populate data

--- src/components/CashFlowGrid.jsx
+++ src/components/CashFlowGrid.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react'
+import CashFlowForm from './CashFlowForm'
 
-export default function CashFlowGrid({ calendarData, cashFlowData, startDateCF }) {
+export default function CashFlowGrid({ calendarData, cashFlowData, cashFlowForm, startDateCF }) {
 const grid = []
+for (let x = 1; x <= 20; x++) {
+  grid.push(x);
+  console.log(grid)
+}
 
 return (
   <div className="grid grid-cols-4">
@@ -9,12 +14,19 @@ return (
   <div className="col-span-1" id="cashflow-heading">Account 1</div>
   <div className="col-span-1" id="cashflow-heading">Account 2</div>
   <div className="col-span-1" id="cashflow-heading">Total</div>
-  {grid.map(() => {
+  {grid.map((startDateCF) => {
+    return (
     <div key={startDateCF}>
-      {startDateCF} </div>
+      <div>
+        {startDateCF} 
+      </div>
+    </div>)
+        console.log(startDateCF)
   })}
     {/* Enter cashFlowData here for start and end dates, calendar Data for content */}
+
   </div>
+
 )
 
 }
