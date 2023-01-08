8c746a22a5f725be95bde965dd915f446f96b081
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2023-01-08 16:07:44 -0330

    Update CashFlowGrid.jsx

--- src/components/CashFlowGrid.jsx
+++ src/components/CashFlowGrid.jsx
@@ -1,34 +1,53 @@
 import React, { useState, useEffect } from "react";
-import {months} from './CalendarSchema.json'
+import { months } from "./CalendarSchema.json";
+
+const calendarFilter = (event, day) => {
+  const startDate = new Date(`${event.startDate} 00:00:00`);
+  const endDate = new Date(`${event.endDate} 00:00:00`);
+
+  function datediff(first, second) {
+    return Math.round((second - first) / (1000 * 60 * 60 * 24));
+  }
+
+  if (day < startDate || day > endDate) return false;
+  switch (event.frequency) {
+    case "Once":
+      return day === startDate;
+    case "Weekly":
+      return day === startDate || datediff(startDate, day) % 7 === 0;
+    case "Biweekly":
+      return day === startDate || datediff(startDate, day) % 14 === 0;
+    default:
+      throw new Error("invalid frequency: " + event.frequency)
+  }
+};
 
 const calendarDataToTimeline = (calendarData, startDateCF, endDateCF) => {
-  
-  const startDate = new Date(`${startDateCF} 00:00:00`)
-  const endDate = new Date(`${endDateCF} 00:00:00`)
-  const timelineEntries = []
+  const startDate = new Date(`${startDateCF} 00:00:00`);
+  const endDate = new Date(`${endDateCF} 00:00:00`);
+  const timelineEntries = [];
 
-  // Need a starting amount generated for each account
   for (let d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
-    // Subtract/add from starting amount for each account
+    // Subtract/add amount per account based on calendarData
+    const eventsToday = calendarData.filter((event) =>
+      calendarFilter(event, d)
+    ); 
+    console.log(eventsToday)
     timelineEntries.push({
-      date: new Date(d.getTime()),
+      date: new Date(d.getTime()), 
       // Need to push with running totals of accounts
       // forEach method may help, can iterate per day and record the amounts a function based on amount and date
-    })
+    });
   }
   return timelineEntries;
-}
-
-
+};
 
 export default function CashFlowGrid({ calendarData, cashFlowData, watch }) {
-
-  const startDate = watch("startDateCF")
-  const endDate = watch("endDateCF")
+  const startDate = watch("startDateCF");
+  const endDate = watch("endDateCF");
   useEffect(() => {
-    console.log(startDate, endDate)
-  }
-  )
+    console.log(startDate, endDate);
+  });
 
   // Boundaries of Date Range of Grid (eventually have follow the month view changes made in Calendar)
 
@@ -55,25 +74,21 @@ export default function CashFlowGrid({ calendarData, cashFlowData, watch }) {
     return new Date(year, month, 0).getDate();
   }
 
-  const dateSpan = []
-
-  // Replace below for loop with one that only pushes values onto dates that have data in the Calendar
-  for (let x = 1; x <= daysInMonth(thisYear, currentMonth + 1); x++) {
-    dateSpan.push(x);
-  }
-
-  
-// Content of Grid
+  // Content of Grid
 
   const grid = [
-    { accountName1: "Acc 1", cost1: "100"},
-    { accountName2: "Acc 2", cost2: "50"},
-    { accountName3: "Acc 3", cost3: "10"},
-  ]
-  
-  const timelineEntries = calendarDataToTimeline(calendarData, startDate, endDate)
-  console.log(timelineEntries)
-  
+    { accountName1: "Acc 1", cost1: "100" },
+    { accountName2: "Acc 2", cost2: "50" },
+    { accountName3: "Acc 3", cost3: "10" },
+  ];
+
+  const timelineEntries = calendarDataToTimeline(
+    calendarData,
+    startDate,
+    endDate
+  );
+  console.log(timelineEntries);
+
   return (
     <div className="grid grid-cols-5">
       <div className="col-span-1" id="cashflow-heading">
@@ -91,10 +106,9 @@ export default function CashFlowGrid({ calendarData, cashFlowData, watch }) {
       <div className="col-span-1" id="cashflow-heading">
         Total
       </div>
-      {dateSpan.map(() => {
-        return (
-            <div className={`${columnChooser[0]}`}></div>
-        )})}
+      {timelineEntries.map(({date}) => {
+        return <div className={`${columnChooser[0]}`}>{date.toISOString().split("T")[0]}</div>;
+      })}
       {/* Enter cashFlowData here for start and end dates, calendar Data for content */}
     </div>
   );
